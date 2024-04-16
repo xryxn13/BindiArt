0b33730bfa848de721e2e7f7377ae68da22adbbf
ARYAN PARMAR <82685818+xryxn13@users.noreply.github.com>
2024-04-16 17:54:26 +0530

    Update bindifinallstreamlit.py

--- bindifinallstreamlit.py
+++ bindifinallstreamlit.py
@@ -3,20 +3,140 @@ from PIL import Image,ImageSequence
 import numpy as np
 import io
 
-def process_image(source_image, matrix_size):
-    # Open GIF images
-    gif1 = Image.open('BindiGifs/1.gif')
-    gif2 = Image.open('BindiGifs/2.gif')
-    gif3 = Image.open('BindiGifs/3.gif')
-    gif4 = Image.open('BindiGifs/4.gif')
-    gif5 = Image.open('BindiGifs/5.gif')
+def process_image(source_image, matrix_size,selected_color):
+    REDgif1 = Image.open('BindiGifs/RED/1.gif')
+    REDgif2 = Image.open('BindiGifs/RED/2.gif')
+    REDgif3 = Image.open('BindiGifs/RED/3.gif')
+    REDgif4 = Image.open('BindiGifs/RED/4.gif')
+    REDgif5 = Image.open('BindiGifs/RED/5.gif')
+    RED1 = Image.open("BindiImages/RED/1.png")
+    RED2 = Image.open("BindiImages/RED/2.png")
+    RED3 = Image.open("BindiImages/RED/3.png")
+    RED4 = Image.open("BindiImages/RED/4.png")
+    RED5 = Image.open("BindiImages/RED/5.png")
+    PINKgif1 = Image.open('BindiGifs/PINK/1.gif')
+    PINKgif2 = Image.open('BindiGifs/PINK/2.gif')
+    PINKgif3 = Image.open('BindiGifs/PINK/3.gif')
+    PINKgif4 = Image.open('BindiGifs/PINK/4.gif')
+    PINKgif5 = Image.open('BindiGifs/PINK/5.gif')
+    PINK1 = Image.open("BindiImages/PINK/1.png")
+    PINK2 = Image.open("BindiImages/PINK/2.png")
+    PINK3 = Image.open("BindiImages/PINK/3.png")
+    PINK4 = Image.open("BindiImages/PINK/4.png")
+    PINK5 = Image.open("BindiImages/PINK/5.png")
+    BLUEgif1 = Image.open('BindiGifs/BLUE/1.gif')
+    BLUEgif2 = Image.open('BindiGifs/BLUE/2.gif')
+    BLUEgif3 = Image.open('BindiGifs/BLUE/3.gif')
+    BLUEgif4 = Image.open('BindiGifs/BLUE/4.gif')
+    BLUEgif5 = Image.open('BindiGifs/BLUE/5.gif')
+    BLUE1 = Image.open("BindiImages/BLUE/1.png")
+    BLUE2 = Image.open("BindiImages/BLUE/2.png")
+    BLUE3 = Image.open("BindiImages/BLUE/3.png")
+    BLUE4 = Image.open("BindiImages/BLUE/4.png")
+    BLUE5 = Image.open("BindiImages/BLUE/5.png")
+    BLACKgif1 = Image.open('BindiGifs/BLACK/1.gif')
+    BLACKgif2 = Image.open('BindiGifs/BLACK/2.gif')
+    BLACKgif3 = Image.open('BindiGifs/BLACK/3.gif')
+    BLACKgif4 = Image.open('BindiGifs/BLACK/4.gif')
+    BLACKgif5 = Image.open('BindiGifs/BLACK/5.gif')
+    BLACK1 = Image.open("BindiImages/BLACK/1.png")
+    BLACK2 = Image.open("BindiImages/BLACK/2.png")
+    BLACK3 = Image.open("BindiImages/BLACK/3.png")
+    BLACK4 = Image.open("BindiImages/BLACK/4.png")
+    BLACK5 = Image.open("BindiImages/BLACK/5.png")
+    ORANGEgif1 = Image.open('BindiGifs/ORANGE/1.gif')
+    ORANGEgif2 = Image.open('BindiGifs/ORANGE/2.gif')
+    ORANGEgif3 = Image.open('BindiGifs/ORANGE/3.gif')
+    ORANGEgif4 = Image.open('BindiGifs/ORANGE/4.gif')
+    ORANGEgif5 = Image.open('BindiGifs/ORANGE/5.gif')
+    ORANGE1 = Image.open("BindiImages/ORANGE/1.png")
+    ORANGE2 = Image.open("BindiImages/ORANGE/2.png")
+    ORANGE3 = Image.open("BindiImages/ORANGE/3.png")
+    ORANGE4 = Image.open("BindiImages/ORANGE/4.png")
+    ORANGE5 = Image.open("BindiImages/ORANGE/5.png")
+    GREENgif1 = Image.open('BindiGifs/GREEN/1.gif')
+    GREENgif2 = Image.open('BindiGifs/GREEN/2.gif')
+    GREENgif3 = Image.open('BindiGifs/GREEN/3.gif')
+    GREENgif4 = Image.open('BindiGifs/GREEN/4.gif')
+    GREENgif5 = Image.open('BindiGifs/GREEN/5.gif')
+    GREEN1 = Image.open("BindiImages/GREEN/1.png")
+    GREEN2 = Image.open("BindiImages/GREEN/2.png")
+    GREEN3 = Image.open("BindiImages/GREEN/3.png")
+    GREEN4 = Image.open("BindiImages/GREEN/4.png")
+    GREEN5 = Image.open("BindiImages/GREEN/5.png")
+    if selected_color=='Red':
+        gif1 = REDgif1
+        gif2 = REDgif2
+        gif3 = REDgif3
+        gif4 = REDgif4
+        gif5 = REDgif5
 
-    # Open dice images
-    die_one = Image.open("BindiImages/1.png")
-    die_two = Image.open("BindiImages/2.png")
-    die_three = Image.open("BindiImages/3.png")
-    die_four = Image.open("BindiImages/4.png")
-    die_five = Image.open("BindiImages/5.png")
+        die_one = RED1
+        die_two = RED2
+        die_three = RED3
+        die_four = RED4
+        die_five = RED5
+    
+    if selected_color=='black':
+        gif1 = BLACKgif1
+        gif2 = BLACKgif2
+        gif3 = BLACKgif3
+        gif4 = BLACKgif4
+        gif5 = BLACKgif5
+
+        die_one = BLACK1
+        die_two = BLACK2
+        die_three = BLACK3
+        die_four = BLACK4
+        die_five = BLACK5
+    if selected_color=='blue':
+        gif1 = BLUEgif1
+        gif2 = BLUEgif2
+        gif3 = BLUEgif3
+        gif4 = BLUEgif4
+        gif5 = BLUEgif5
+
+        die_one = BLUE1
+        die_two = BLUE2
+        die_three = BLUE3
+        die_four = BLUE4
+        die_five = BLUE5
+    if selected_color=='green':
+        gif1 = GREENgif1
+        gif2 = GREENgif2
+        gif3 = GREENgif3
+        gif4 = GREENgif4
+        gif5 = GREENgif5
+
+        die_one = GREEN1
+        die_two = GREEN2
+        die_three = GREEN3
+        die_four = GREEN4
+        die_five = GREEN5
+    if selected_color=='orange':
+        gif1 = ORANGEgif1
+        gif2 = ORANGEgif2
+        gif3 = ORANGEgif3
+        gif4 = ORANGEgif4
+        gif5 = ORANGEgif5
+
+        die_one = ORANGE1
+        die_two = ORANGE2
+        die_three = ORANGE3
+        die_four = ORANGE4
+        die_five = ORANGE5
+    if selected_color=='pink':
+        gif1 = PINKgif1
+        gif2 = PINKgif2
+        gif3 = PINKgif3
+        gif4 = PINKgif4
+        gif5 = PINKgif5
+
+        die_one = PINK1
+        die_two = PINK2
+        die_three = PINK3
+        die_four = PINK4
+        die_five = PINK5
 
     number_of_frames = min(gif1.n_frames, gif2.n_frames, gif3.n_frames, gif4.n_frames, gif5.n_frames)
     frames = []
@@ -121,6 +241,8 @@ def convert_image_to_bytes(img):
     return img_byte_arr.getvalue()
 
 def main():
+    colors = {"Black": "black", "Blue": "blue", "Green": "green",
+          "Pink": "pink", "Red": "red", "Orange": "orange"}
     col1, col2 = st.columns(2)
     with col1:
         st.title("Bindi Art by CCL, IIT Gandhinagar")    
@@ -131,11 +253,13 @@ def main():
 
     # Slider for selecting matrix size
     matrix_size = st.slider("Select matrix size:", min_value=1, max_value=70, value=60)
+    selected_color = st.selectbox("Choose a Color", list(colors.keys()))
+    color_hex = colors[selected_color]
 
     if uploaded_image is not None:
         # Process the image
         source_image = Image.open(uploaded_image)
-        processed_image, counts = process_image(source_image, matrix_size)
+        processed_image, counts = process_image(source_image, matrix_size,color_hex)
         with open("outputGIF.gif", "rb") as f:
             gif_bytes = f.read()
         # Display the processed image
